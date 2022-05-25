980e6e7453866aa3e5c15764add302fc1de4a7fc
VECTORITCGROUP\apellejero <apellejero@ext.vectoritcgroup.com>
2022-05-25 13:50:09 +0200

    class 5, part 1

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,15 +1,23 @@
 import { useState, useEffect, useRef } from 'react';
 
-export default function useNearScreen({ distance = '100px' } = {}) {
+export default function useNearScreen({
+  distance = '100px',
+  externalRef,
+  once = true,
+} = {}) {
   const [isNearScreen, setShow] = useState(false);
   const fromRef = useRef();
 
   useEffect(function () {
+    const element = externalRef ? externalRef.current : fromRef.current;
+
     const onChange = (entries) => {
       const el = entries[0];
       if (el.isIntersecting) {
         setShow(true);
-        observer.disconnect();
+        once && observer.disconnect();
+      } else {
+        !once && setShow(false);
       }
     };
 
@@ -17,9 +25,9 @@ export default function useNearScreen({ distance = '100px' } = {}) {
       rootMargin: distance,
     });
 
-    observer.observe(fromRef.current);
+    if (element) observer.observe(element);
 
-    return () => observer.disconnect();
+    return () => observer && observer.disconnect();
   });
 
   return { isNearScreen, fromRef };
--- src/pages/SearchResult/index.js
+++ src/pages/SearchResult/index.js
@@ -1,17 +1,32 @@
 import Spinner from 'components/Spinner';
 import ListOfGifts from 'components/ListOfGifts/ListOfGifts';
 import { useGifts } from 'hooks/useGifts';
+import useNearScreen from 'hooks/useNearScreen';
+import { useCallback, useEffect, useRef } from 'react';
+import debounce from 'just-debounce-it';
 
 export default function SearchResults({ params }) {
   const { keyword } = params;
   const { loading, gifts, setPage } = useGifts({ keyword });
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false,
+  });
 
-  const handleNextPage = () => setPage((prevPage) => prevPage + 1);
+  const debounceHandleNextPage = useCallback(
+    debounce(() => setPage(prevPage => prevPage + 1), 200),
+    [setPage]
+  );
+
+  useEffect(() => {
+    if (isNearScreen) debounceHandleNextPage();
+  }, [debounceHandleNextPage, isNearScreen]);
 
   return (
     <>
       {loading ? <Spinner /> : <ListOfGifts gifts={gifts} />} <br />
-      <button onClick={handleNextPage}>Next Page</button>
+      <div id='visor' ref={externalRef}></div>
     </>
   );
 }
