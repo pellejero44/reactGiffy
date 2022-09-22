9834c39d197e12d69ae038378552b6403b4d08d1
Angel Pellejero Gomes <apellejero@ext.vectoritcgroup.com>
2022-09-22 16:04:42 +0200

    feat: component form redenring once

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,23 @@
-import { useState } from 'react';
-import { useLocation } from 'wouter';
 import ListOfGifts from 'components/ListOfGifts/ListOfGifts';
-import { useGifts } from 'hooks/useGifts';
+import SearchForm from 'components/SearchForm';
 import LazyTrending from 'components/TrendingSearches';
+import { useGifts } from 'hooks/useGifts';
+import { useCallback } from 'react';
+import { useLocation } from 'wouter';
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('');
   const [path, pushLocation] = useLocation();
   const { loading, gifts } = useGifts({ keyword: 'matrix' });
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
+  const handleSubmit = useCallback(({keyword}) => {
     pushLocation(`/search/${keyword}`);
-  };
+  }, [pushLocation]);
 
-  const handleChange = (evt) => {
-    setKeyword(evt.target.value);
-  };
+  
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Search a gif here ...'
-          value={keyword}
-          onChange={handleChange}
-        />
-        <button>Search</button>
-      </form>
+      <SearchForm onSubmit={handleSubmit}/>
       <h3 className='App-title'>Last search</h3>
       <ListOfGifts gifts={gifts} />
       <h3 className='App-title'>The most popular gifts</h3>
